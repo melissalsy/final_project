b1b6ca4147cda95ac17fcad386d157989f7b577d
melissalsy <melissaleesy@gmail.com>
2016-03-13 15:40:50 -0400

    changes

--- app/site/ctrl/shopCtrl.js
+++ app/site/ctrl/shopCtrl.js
@@ -1,52 +1,62 @@
 app.controller('shopCtrl', ShopCtrl);
 
-function ShopCtrl($scope, $state, orderSrv, api) {
-	var ctrl = this;
-	ctrl.state = $state;
-	ctrl.scope = $scope;
-	ctrl.orderSrv = orderSrv;
-	ctrl.api = api;
-
-    $scope.product = {
-        items: [{
-            qty: this.qty,
-            name: 'Portable Outlet',
-            cost: 100,
-            total: 0}]
+function ShopCtrl($state, $scope, orderSrv, api) {
+    var ctrl = this; 
+    ctrl.state = $state;
+    ctrl.scope = $scope;
+    ctrl.orderSrv = orderSrv;
+    ctrl.api = api;
+    ctrl.product = {
+        name: 'Portable Outlet',
+        qty: 1,
+        cost: 100.00
     };
+    ctrl.cart = [];
+    ctrl.customerInfo = {};
+    $scope.$watch(function(){
+        return orderSrv.cart;
+    }, function (newValue, oldValue) {
+        ctrl.cart = newValue;
+        if(orderSrv.cart.data != ctrl.product){
+            console.log(orderSrv.cart);
+            // ctrl.product = orderSrv.cart;
+            console.log(ctrl.product);
+        }
+    });
+    
+    $scope.$watch(function(){
+        return orderSrv.newCustomer;
+    }, function (newValue, oldValue) {
+        ctrl.customerInfo= newValue;
+    });
 }
-
+ShopCtrl.prototype.eachTotal = function(){
+    var ctrl = this; 
+    ctrl.totalCart = ctrl;product.quantity * ctrl.product.cost
+}
 ShopCtrl.prototype.removeItem = function(index) {
-    this.scope.product.items.splice(index, 1);
-   	this.state.go('product');
-    }
-
-ShopCtrl.prototype.total = function() {
-        var total = 0;
-        angular.forEach(this.scope.product.items, function(item) {
-            total += item.qty * item.cost;
-        })
-
-        return total;
-    }
-
-ShopCtrl.prototype.checkout = function(){
-	var order = this.scope.product;
-	order.items[0].total = order.items[0].qty  * order.items[0].cost;
-	this.orderSrv.addOrder(order);
-	console.log(this.orderSrv.orders);
-	this.state.go('checkout');
+    var ctrl = this; 
+    ctrl.product.splice(index, 1);
+    ctrl.state.go('product');
 }
-
+ShopCtrl.prototype.total = function() {
+    var ctrl = this;
+    var total = 0;
+    for (var i=0; i<cart.length; i++) {
+        total += ctrl.cart[i].quantity;
+    };
+    console.log(total);
+}
 ShopCtrl.prototype.addToCart = function(){
     var ctrl = this;
     ctrl.orderSrv.addProducts(ctrl.product);
+    ctrl.state.go('cart');
 }
-
+// ShopCtrl.prototype.submitOrdder = function (){
+// }
 ShopCtrl.prototype.reviewOrder = function(){
     var ctrl = this; 
-
-    var customer = {
+    ctrl.customer = {
         firstName: ctrl.firstName,
         lastName: ctrl.lastName,
         email: ctrl.email,
@@ -56,8 +66,18 @@ ShopCtrl.prototype.reviewOrder = function(){
         province: ctrl.province,
         postal: ctrl.postal
     }
-
-    console.log(customer);
-    ctrl.orderSrv.newCustomer = customer;
-    ctrl.$state.go('review');
+    ctrl.orderSrv.addCustomer(ctrl.customer);
+    ctrl.state.go('review');
+}
+ShopCtrl.prototype.submitOrder = function(){
+    var ctrl = this;
+    ctrl.order = {
+       customer: ctrl.customer,
+       cart: ctrl.cart,
+       total: '',
+       tax:'',
+       final_total:'',
+   };
+    ctrl.orderSrv.addOrder(ctrl.order);
+    ctrl.$state.go('shop.lastpage');
 }
--- app/site/services/orderSrv.js
+++ app/site/services/orderSrv.js
@@ -1,43 +1,44 @@
 app.service('orderSrv', OrderService);
 
 function OrderService($state, api){
-	var srv = this;
-	srv.api = api;
-	srv.state = $state;
-	srv.orders = [];
-	srv.cart = [];
-	srv.newCustomer;
+    var srv = this;
+    srv.api = api;
+    srv.state = $state;
+    srv.orders = [];
+    srv.cart = [];
+    srv.newCustomer;
 }
-
 OrderService.prototype.addProducts = function (product){
-	var srv = this; 
-	srv.cart.push(product);
-	console.log(srv.cart)
+    var srv = this; 
+    srv.cart.push(product);
 }
 OrderService.prototype.getOrders = function(){
-	var srv = this;
-	return srv.api.request('/orders', {}, 'GET')
-		.then(function(res){
-			console.log(res);
-			srv.orders = res.data;
-			return res.data;
-		}, function(res){
-			console.log(res);
-			return;
-		})
+    var srv = this;
+    return srv.api.request('/orders', {}, 'GET')
+        .then(function(res){
+            console.log(res);
+            srv.orders = res.data;
+            return res.data;
+        }, function(res){
+            console.log(res);
+            return;
+        })
 }
-
+OrderService.prototype.addCustomer = function(customer){
+    var srv = this; 
+    srv.newCustomer = customer;
+    console.log(srv.newCustomer);
+}
 OrderService.prototype.addOrder = function(order){
-	var srv = this;
-	console.log(order);
-	srv.api.request('/orders',order,'POST')
-	.then(function(res){
-		console.log(res);
-		if(res.status === 200){
-			//order was added successfully
-			srv.orders.push(res.data.order);
-			//srv.state.go('admin');
-
-		}
-	})
+    var srv = this;
+    console.log(order);
+    srv.api.request('/orders',order,'POST')
+    .then(function(res){
+        console.log(res);
+        if(res.status === 200){
+            //order was added successfully
+            srv.orders.push(res.data.order);
+            //srv.state.go('admin');
+        }
+    })
 }
